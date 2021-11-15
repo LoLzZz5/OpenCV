6465f84dbb5e0854bf20c55243b0a0f9639f7085
LoL <zhora.polyakov95@mail.ru>
2021-11-15 10:58:39 +0300

    Google translator added

--- ocv.py
+++ ocv.py
@@ -1,12 +1,18 @@
 import cv2
 import pytesseract
+from googletrans import Translator
 
 pytesseract.pytesseract.tesseract_cmd = '...'
 
-img = cv2.imread('...')
+img = cv2.imread('text.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-print(pytesseract.image_to_string(img))
+text = pytesseract.image_to_string(img)
+
+translator = Translator()
+text = translator.translate(text, dest = 'ru')
+
+print(text.text)
 
 cv2.imshow('Img', img)
 cv2.waitKey(0)
